b48074e6a125b99065fd3a140c0bcab129bf4b86
gilles <gilles@pilloud.fr>
2014-09-23 23:47:46 +0200

    MISSING FILE

--- models/Registration.js
+++ models/Registration.js
@@ -15,7 +15,8 @@ Registration.add({
     availability: {type: Types.Select, options: 'D, SN, ND, C', default: 'disponible', index: true, initial: true, required: true},
     person: {type: Types.Relationship, ref: 'User', index: true, initial: true, required: true},
     competence: {type: Types.Relationship, ref: 'Competence', index: true, initial: true, required: true},
-    eventDate: {type: Types.Relationship, ref: 'EventDate', initial: true, required: true}
+    eventDate: {type: Types.Relationship, ref: 'EventDate', initial: true, required: true},
+    registered: {type: Types.Boolean, initial: true, required: true}
 });
 
 Registration.defaultColumns = 'person, availability,competence,eventDate';
